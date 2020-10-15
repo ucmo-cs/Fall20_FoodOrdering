c8068a1bf3607b399b5ba83789c6b4323eef72ca
Joshua Weber <jlw55080@ucmo.edu>
2020-10-15 17:13:55 -0500

    Updated Code Push
    New connection code and modified login controller as well.

--- src/LoginController.java
+++ src/LoginController.java
@@ -4,6 +4,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -24,11 +25,13 @@ public class LoginController {
     @FXML public TextField textFieldPassword;
     @FXML public Button buttonLogin;
     @FXML public Label labelMessage;
+    @FXML public RadioButton radButtonStudent;
+    @FXML public RadioButton radButtonEmployee;
 
     public void openLogin() throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXML_Files/loginScreen.fxml"));
         loginStage.setTitle("Mule Trough Login");
-        loginStage.setScene(new Scene(root, 300, 200));
+        loginStage.setScene(new Scene(root, 300, 220));
         loginStage.show();
     }
 
@@ -46,41 +49,45 @@ public class LoginController {
 
     public void attemptLogin() throws Exception {
         // Create Variables
-        String username=textFieldUsername.getText();
-        String claimedPassword=textFieldPassword.getText();
-        String id=null,fname=null,lname=null,truePassword=null;
-        Boolean employee=false;
 
-        // Acquire user data
-        String query = "select * from student.student_information where id = '"+username+"';";
+        // Check to ensure that textfields have data.
+        if(textFieldUsername.getText().isEmpty()){ textFieldUsername.setText("Username Required"); }
+        else if (textFieldPassword.getText().isEmpty()) { textFieldPassword.setText("Password Required"); }
+        else {
 
-        CachedRowSet accountData = sqlCommands.readStudentDatabase(query);
+            String username = textFieldUsername.getText();
+            String claimedPassword = textFieldPassword.getText();
+            String id = null, fname = null, lname = null, truePassword = null;
+            Boolean employee = false;
 
-        // Determine if user info was in CachedRow
-        if(!accountData.isBeforeFirst()) {
-            labelMessage.setTextFill(Color.RED);
-            labelMessage.setText("Invalid Username");
-        }
-        else
-        {
-            // Acquire the account's password and than compare to the given password
-            while(accountData.next()){
-                id=accountData.getString("id");
-                fname=accountData.getString("first_name");
-                lname=accountData.getString("last_name");
-                truePassword = accountData.getString("password");
-            }
-            if (claimedPassword.equals(truePassword)){
-                setUser(id,fname,lname,employee);
-                menuController.openMainScreen();
-                closeLogin();
-            }
-            else{
+            // Acquire user data
+            String query = "select * from student.student_information where id = '" + username + "';";
+
+            CachedRowSet accountData = sqlCommands.readDataBase(1, query);
+
+            // Determine if user info was in CachedRow
+            if (!accountData.isBeforeFirst()) {
                 labelMessage.setTextFill(Color.RED);
-                labelMessage.setText("Invalid Password");
+                labelMessage.setText("Invalid Username");
+            } else {
+                // Acquire the account's password and than compare to the given password
+                while (accountData.next()) {
+                    id = accountData.getString("id");
+                    fname = accountData.getString("first_name");
+                    lname = accountData.getString("last_name");
+                    truePassword = accountData.getString("password");
+                }
+                if (claimedPassword.equals(truePassword)) {
+                    setUser(id, fname, lname, employee);
+                    menuController.openMainScreen();
+                    closeLogin();
+                } else {
+                    labelMessage.setTextFill(Color.RED);
+                    labelMessage.setText("Invalid Password");
+                }
             }
-        }
-    }
+        }// Ends else
+    }// Ends attemptLogin
 
     public void attemptloginTest() throws Exception {
         menuController.openMainScreen();
--- src/SQLCommands.java
+++ src/SQLCommands.java
@@ -3,92 +3,41 @@ import javax.sql.rowset.RowSetProvider;
 import java.sql.*;
 
 public class SQLCommands {
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private CachedRowSet cachedRowSet;
 
-    private Connection connection=null;
-    private PreparedStatement statement=null;
-
-    private final String urlRestaurant="jdbc:mysql://stoves-dev.duckdns.org:50931/restaurant?serverTimezone=CST";
-    private final String urlStudent="jdbc:mysql://stoves-dev.duckdns.org:50931/student?serverTimezone=CST";
-    private final String dbDriver="com.mysql.jdbc.Driver";
-    private final String username="table_editor";
-    private final String password="!sleekPanda!";
-
-    // NOTICE!
-    // setConnection and readRestaurantDataBase are still in development.
-    // Will replace both of the other readDB functions when completed.
-    public CachedRowSet setConnection(int db) throws SQLException {
-        String url=null;
-        switch (db){
-            case 1: url=urlRestaurant;
-                    break;
-            case 2: url=urlStudent;
-                    break;
-            default: url="Invalid URL";
-                    break;
-        }
-        CachedRowSet cachedRowset=RowSetProvider.newFactory().createCachedRowSet();
-        cachedRowset.setUrl(url);
-        cachedRowset.setUsername(username);
-        cachedRowset.setPassword(password);
-
-        return cachedRowset;
+    public void createConnection(ConnectionValues connectionValues) throws SQLException {
+        connection=DriverManager.getConnection(connectionValues.urlString,connectionValues.userName,connectionValues.password);
     }
-    public CachedRowSet readDataBase(int dbID, String query) throws Exception {
-        int db=dbID;
-        try {
-            CachedRowSet cachedRowset=setConnection(db);
-            cachedRowset.setCommand(query);
-            statement=connection.prepareStatement(cachedRowset.getCommand());
-            statement.execute();
 
-            return cachedRowset;
+    public void createCachedRowSet(ConnectionValues connectionValues, String query) throws SQLException {
+        cachedRowSet= RowSetProvider.newFactory().createCachedRowSet();
+        cachedRowSet.setUsername(connectionValues.userName);
+        cachedRowSet.setPassword(connectionValues.password);
+        cachedRowSet.setUrl(connectionValues.urlString);
+        cachedRowSet.setCommand(query);
+    }
+
+    public void createPreparedStatement(CachedRowSet cachedRowSet) throws SQLException {
+        preparedStatement=connection.prepareStatement(cachedRowSet.getCommand());
+    }
+
+    public CachedRowSet readDataBase(int dbID, String query) throws Exception {
+        ConnectionValues connectionValues=new ConnectionValues(dbID);
+        createConnection(connectionValues);
+        createCachedRowSet(connectionValues,query);
+        createPreparedStatement(cachedRowSet);
+
+        try {
+            cachedRowSet.execute();
+            return cachedRowSet;
         }catch (Exception e){
             System.err.print("ERROR!\nFunction: readDataBase\nClass: SQLCommands\n");
-            throw e;
+            System.err.print(e);
         }finally {
-            close();
+            connection.close();
         }
-    }
-
-    public CachedRowSet readRestaurantDataBase(String query) throws Exception {
-        try{
-            CachedRowSet cachedRowset=setConnection(1);
-            cachedRowset.setCommand(query);
-            cachedRowset.execute();
-
-            return cachedRowset;
-        }catch (Exception e){
-            throw e;
-        }finally {
-            close();
-        }
-    }
-
-    public CachedRowSet readStudentDatabase(String query) throws Exception {
-        try{
-
-            CachedRowSet cachedRowset=setConnection(2);
-            cachedRowset.setCommand(query);
-            cachedRowset.execute();
-
-            return cachedRowset;
-        }catch (Exception e){
-            throw e;
-        }finally {
-            close();
-        }
-    }
-
-    public void close(){
-        try{
-            if(statement!=null){
-                statement.close();
-            }
-            if(connection!=null){
-                connection.close();
-            }
-        } catch (Exception e){
-
-        }
+        return cachedRowSet;
     }
 }
