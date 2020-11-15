12dd34ce2cb6b5a7de01a37c866f8f212dd9ad48
Alex <e.a_madera@hotmail.com>
2020-11-15 10:21:23 -0600

    map working

--- src/Controllers/SpinPizzaController.java
+++ src/Controllers/SpinPizzaController.java
@@ -5,16 +5,28 @@ import Models.RestaurantModel;
 import Models.SQLCommands;
 import Queries.RestaurantQueries;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import javax.sql.rowset.CachedRowSet;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public class SpinPizzaController extends RestaurantBaseController{
 
     @FXML Tab tabMain;
     @FXML Tab tabDessert;
     @FXML Tab tabDrinks;
+    @FXML TableView<FoodMenuItem>tableViewMain;
+    @FXML TableView<FoodMenuItem>tableViewDesserts;
+    @FXML TableView<FoodMenuItem>tableViewDrinks;
     @FXML TableColumn columnMainName;
     @FXML TableColumn columnMainPrice;
     @FXML TableColumn columnMainAvailable;
@@ -29,20 +41,39 @@ public class SpinPizzaController extends RestaurantBaseController{
     private final static int RESTAURANT_ID = 5;
 
     public void initialize() throws Exception {
-        String getFoodsQuery = RestaurantQueries.getFoodsByRestaurantIDQuery(String.valueOf(RESTAURANT_ID));
         SQLCommands sqlCommands = new SQLCommands();
-        CachedRowSet foods = sqlCommands.readDataBase(1, getFoodsQuery);
-        while(foods.next())
-        {
-            FoodMenuItem item = new FoodMenuItem(
-                    foods.getString(1),
-                    foods.getString(2),
-                    foods.getString(3),
-                    foods.getString(4),
-                    foods.getString(5),
-                    foods.getString(6));
-            this.spin.appendFood(item);
+        buildMenu(RESTAURANT_ID);
+        fillTable();
+    }
+    public void fillTable() throws SQLException {
+        List<FoodMenuItem> spinFood = this.menuModel.getFoods();
+        columnMainName.setCellValueFactory(new PropertyValueFactory<FoodMenuItem, String>("name"));
+        columnMainPrice.setCellValueFactory(new PropertyValueFactory<FoodMenuItem, String>("price"));
+        columnMainAvailable.setCellValueFactory(new PropertyValueFactory<FoodMenuItem, String>("available"));
+
+        columnDessertName.setCellValueFactory(new PropertyValueFactory<FoodMenuItem, String>("name"));
+        columnDessertPrice.setCellValueFactory(new PropertyValueFactory<FoodMenuItem, String>("price"));
+        columnDessertAvailable.setCellValueFactory(new PropertyValueFactory<FoodMenuItem, String>("available"));
+
+        columnDrinksName.setCellValueFactory(new PropertyValueFactory<FoodMenuItem, String>("name"));
+        columnDrinksPrice.setCellValueFactory(new PropertyValueFactory<FoodMenuItem, String>("price"));
+        columnDrinksAvailable.setCellValueFactory(new PropertyValueFactory<FoodMenuItem, String>("available"));
+
+        for (FoodMenuItem f : spinFood) {
+            switch (f.type) {
+                case "main" -> tableViewMain.getItems().add(f);
+                case "dessert" -> tableViewDesserts.getItems().add(f);
+                case "drink" -> tableViewDrinks.getItems().add(f);
+            }
         }
-        this.spin.showFoods();
+    }
+    public void OpenCheckout() throws IOException {
+        Parent checkout = FXMLLoader.load(getClass().getResource("FXML_Files/CheckoutScreen.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(checkout,1000,700);
+        scene.getStylesheets().add(getClass().getResource("FXML_Files/test.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("FXML_Files/login.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
     }
 }
