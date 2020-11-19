ba95870e8a1f7b0a2268c0478fec8f2e5a0b8874
Joshua Weber <63745585+JOHONDAR@users.noreply.github.com>
2020-11-19 10:03:12 -0600

    New Login Controller Logic
    Now with the ability to log into employee view

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -1,14 +1,19 @@
 package Controllers;
 
+import Queries.LoginQueries;
 import Models.User;
 import Models.SQLCommands;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import javax.sql.rowset.CachedRowSet;
+import java.sql.ResultSet;
 
 public class LoginController {
 
@@ -23,8 +28,9 @@ public class LoginController {
     @FXML public TextField textFieldUsername;
     @FXML public TextField textFieldPassword;
     @FXML public Button buttonLogin;
-    @FXML public Button buttonStudent;
-    @FXML public Button buttonEmployee;
+
+    @FXML public ToggleButton toggleButtonStudent;
+    @FXML public ToggleButton toggleButtonEmployee;
     @FXML public Label labelMessage;
 
     public void closeLogin(){
@@ -77,40 +83,85 @@ public class LoginController {
     }
 
     public void attemptLogin() throws Exception {
-        // Check to ensure that the textfields have acceptable contents.
-        if (validateLoginInfo()==false){return;}
+        // Create Variables
+
+        // Check to ensure that textfields have data.
+        if (textFieldUsername.getText().isEmpty()) { textFieldUsername.setText("Username Required"); return;}
+        if (textFieldPassword.getText().isEmpty()) { textFieldPassword.setText("Password Required"); return;}
 
         // Create Variables
         String username = textFieldUsername.getText();
         String claimedPassword = textFieldPassword.getText();
         String id = null, fname = null, lname = null, truePassword = null;
-        Boolean employee = false;
+        boolean restaurant_user = false;
+        boolean credentials_good = false;
 
-        // Acquire user data
-        String query = "select * from student.student_information where id = '" + username + "';";
-        CachedRowSet accountData = sqlCommands.readDataBase(1, query);
+        // Check if given username exists in student table
+        if (!StudentUserExists(username)) {
 
-        // Determine if user info was in CachedRow
-        if (!accountData.isBeforeFirst()) {
-            displayMessage("Login Info Not found");
-        } else {
-            // Acquire the account's password and than compare to the given password
-            while (accountData.next()) {
-                id = accountData.getString("id");
-                fname = accountData.getString("first_name");
-                lname = accountData.getString("last_name");
-                truePassword = accountData.getString("password");
-            }
-            // If the passwords match and the user information is valid
-            if (claimedPassword.equals(truePassword)) {
-                setUser(id, fname, lname, employee);
-                labelMessage.setVisible(false);
-                mainStageController.openFrontScreen();
-                closeLogin();
-            } else {
-                displayMessage("Login Info Not found");
+            // If username does not exist in student table, check if it exists in the restaurant users table
+            if(RestaurantUserExists(username))
+                restaurant_user = true;
+            else
+            {
+                System.err.println("User does not exist");
+                displayMessage("Invalid Username");
             }
         }
+
+        if (!verifyPassword(username, claimedPassword, restaurant_user)) {
+            System.err.println("Invalid Password");
+            displayMessage("Invalid Password");
+        }
+        else
+            credentials_good = true;
+
+        System.out.printf("Restaurant User: %s\nSucessfull Login: %s\n", String.valueOf(restaurant_user),String.valueOf(credentials_good));
+        if(credentials_good)
+        {
+            // Acquire user data
+            String query = LoginQueries.getUserInfoQuery(username);
+            CachedRowSet accountData = sqlCommands.readDataBase(2, query);
+
+//            accountData.next();
+//            id = accountData.getString("id");
+//            fname = accountData.getString("first_name");
+//            lname = accountData.getString("last_name");
+//            System.out.println(String.format("User: %s, %s: %s", lname, fname, id));
+//            setUser(id, fname, lname, restaurant_user);
+
+            if(restaurant_user==false) {
+                mainStageController.openFrontScreen();
+            } else if(restaurant_user==true){
+                mainStageController.openEmployeeView();
+            }
+            closeLogin();
+        }
+    }
+
+    private boolean verifyPassword(String username, String claimedPassword, boolean restaurant) throws Exception {
+        final String query;
+        if(!restaurant)
+            query = LoginQueries.getStudentPasswordQuery(username);
+        else
+            query = LoginQueries.getRestaurantPasswordQuery(username);
+
+        ResultSet result = DatabaseController.readStudentLoginDatabase(query);
+        result.next();
+        return result.getString(1).equals(claimedPassword);
+    }
+
+    private boolean StudentUserExists(String username) throws Exception {
+        String query = LoginQueries.StudentUserExistsQuery(username);
+        ResultSet result = DatabaseController.readStudentLoginDatabase(query);
+        result.next();
+        return result.getString(1).equals("1");
+    }
+    private boolean RestaurantUserExists(String username) throws Exception {
+        String query = LoginQueries.RestaurantUserExistsQuery(username);
+        ResultSet result = DatabaseController.readStudentLoginDatabase(query);
+        result.next();
+        return result.getString(1).equals("1");
     }
 
     public void attemptloginTest() throws Exception {
