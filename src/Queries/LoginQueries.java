7d97d7b2a7df8efdf6bcb94cb8b3080d0f01504c
Joshua Weber <63745585+JOHONDAR@users.noreply.github.com>
2020-11-19 10:07:51 -0600

    LoginQueries Update
    Stovall did a lot of work that I'm not smart enough to understand. The new methods look sharp though so I'm not complaining.

--- src/Queries/LoginQueries.java
+++ src/Queries/LoginQueries.java
@@ -1,34 +1,54 @@
 package Queries;
 
 public class LoginQueries {
-    public static String IDExistsQuery(String id)
+    public static String StudentUserExistsQuery(String id)
     {
         return String.format(
                 "SELECT\n" +
-                "  COUNT(1)\n" +
-                "FROM\n" +
-                "  login.user\n" +
-                "WHERE\n" +
-                "   id = '%s';", id);
+                        "  COUNT(1)\n" +
+                        "FROM\n" +
+                        "  `login`.`user`\n" +
+                        "WHERE\n" +
+                        "   `id` = '%s';", id);
     }
-    public static String getPasswordQuery(String id)
+    public static String RestaurantUserExistsQuery(String login_name)
     {
         return String.format(
                 "SELECT\n" +
-                "   password\n" +
-                "FROM\n" +
-                "   login.user\n" +
-                "WHERE\n" +
-                "id = '%s';", id);
+                        "  COUNT(1)\n" +
+                        "FROM\n" +
+                        "  `login`.`restaurant`\n" +
+                        "WHERE\n" +
+                        "   `login_name` = '%s';", login_name);
     }
-    public static String getUserInfo(String id)
+    public static String getStudentPasswordQuery(String id)
     {
         return String.format(
                 "SELECT\n" +
-                "   *\n" +
-                "FROM\n" +
-                "   student.student_information\n" +
-                "WHERE\n" +
-                "   id = '%s';",id);
+                        "   `password`\n" +
+                        "FROM\n" +
+                        "   `login`.`user`\n" +
+                        "WHERE\n" +
+                        "`id` = '%s';", id);
+    }
+    public static String getRestaurantPasswordQuery(String login_name)
+    {
+        return String.format(
+                "SELECT\n" +
+                        "   `password`\n" +
+                        "FROM\n" +
+                        "   `login`.`restaurant`\n" +
+                        "WHERE\n" +
+                        "`login_name` = '%s';", login_name);
+    }
+    public static String getUserInfoQuery(String id)
+    {
+        return String.format(
+                "SELECT\n" +
+                        "   *\n" +
+                        "FROM\n" +
+                        "   `student`.`student_information`\n" +
+                        "WHERE\n" +
+                        "   `id` = '%s';",id);
     }
 }
